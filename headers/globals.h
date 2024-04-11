23ab1289ad1b99f51f0dc2115a8ef5d6f1fae047
Michael Ripa <michaelripa@cmail.carleton.ca>
2024-04-11 16:36:59 -0400

    Added numerous Enums for various program states

--- headers/globals.h
+++ headers/globals.h
@@ -2,3 +2,9 @@
 
 extern const int TICKS_PER_SECOND;
 enum class SessionStatus { NotStarted, InProgress, Paused, Completed };
+enum class Light { Red, Green, Blue, Off };
+enum class Device { On, Off};
+enum class Battery { Dead, Alive, Charging};
+enum class Headset { Connected, Disconnected};
+enum class Screen {MainMenu, SessionScreen SessionPaused, SessionStopped, SessionLogs, TimeAndDate, ScreenOff, Admin, PC};
+enum class Band { Alpha, Beta, Delta, Theta};
