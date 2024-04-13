da4782522f4d393234794745ab5b5d679e96afc8
uvernes <uvernes@gmail.com>
2024-04-13 18:26:26 -0400

    Add connections to all interactable GUI widgets.

--- src/main-window.cpp
+++ src/main-window.cpp
@@ -13,12 +13,36 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->setupUi(this);
 
-    // ---- Connections ----
+    // ---- Non-GUI connections ----
     connect(model, SIGNAL(modelChanged()), this, SLOT(render()));
 
+    // --- GUI connections ---
+    connect(ui->startSessionButton, SIGNAL(released()), this, SLOT(handleStartSessionButtonPressed()));
+    connect(ui->sessionLogsButton, SIGNAL(released()), this, SLOT(handleSessionLogsButtonPressed()));
+    connect(ui->timeAndDateButton, SIGNAL(released()), this, SLOT(handleTimeAndDateButtonPressed()));
+    connect(ui->inSessionPauseButton, SIGNAL(released()), this, SLOT(handleInSessionPauseButtonPressed()));
+    connect(ui->inSessionStartButton, SIGNAL(released()), this, SLOT(handleInSessionStartButtonPressed()));
+    connect(ui->inSessionStopButton, SIGNAL(released()), this, SLOT(handleInSessionStopButtonPressed()));
+    connect(ui->sessionCompleteBackButton, SIGNAL(released()), this, SLOT(handleSessionCompleteBackButtonPressed()));
+    connect(ui->sessionIncompleteBackButton, SIGNAL(released()), this, SLOT(handleSessionIncompleteBackButtonPressed()));
+    connect(ui->sessionLogsBackButton, SIGNAL(released()), this, SLOT(handleSessionLogsBackButtonPressed()));
+    connect(ui->sessionLogsClearAllButton, SIGNAL(released()), this, SLOT(handleSessionLogsClearAllButtonPressed()));
+    connect(ui->sessionLogsUploadAllButton, SIGNAL(released()), this, SLOT(handleSessionLogsUploadAllButtonPressed()));
+    connect(ui->saveDateAndTimeChangesButton, SIGNAL(released()), this, SLOT(handleSaveDateAndTimeChangesButtonPressed()));
+    connect(ui->dateAndTimeBackButton, SIGNAL(released()), this, SLOT(handleDateAndTimeBackButtonPressed()));
+    connect(ui->powerButton, SIGNAL(released()), this, SLOT(handlePowerButtonPressed()));
 
+    connect(ui->computerSessionsList, SIGNAL(currentIndexChanged(int)), this, SLOT(handleComputerSessionSelectedChanged()));
+    connect(ui->computerSelectSiteDropdown, SIGNAL(currentIndexChanged(int)), this, SLOT(handleComputerSiteSelectedChanged()));
 
-    // Testing
+    connect(ui->applyBandRangeToAllSitesButton, SIGNAL(released()), this, SLOT(handleApplyBandRangeToAllSitesButtonPressed()));
+    connect(ui->chargeBatteryButton, SIGNAL(released()), this, SLOT(handleChargeBatteryButtonPressed()));
+    connect(ui->connectButton, SIGNAL(released()), this, SLOT(handleConnectButtonPressed()));
+    connect(ui->disconnectButton, SIGNAL(released()), this, SLOT(handleDisconnectButtonPressed()));
+    connect(ui->waveSettingsBandRangeDropdown, SIGNAL(currentIndexChanged(int)), this, SLOT(handleWaveSettingsBandRangeSelectedChanged()));
+    connect(ui->waveSettingsBandRangeDropdown, SIGNAL(currentIndexChanged(int)), this, SLOT(handleWaveSettingsSiteSelectedChanged()));
+
+    // --- Debugging-only connections (delete later) ---
     connect(ui->helloWorldButton, SIGNAL(released()), this, SLOT(helloWorld()));
 
 }
