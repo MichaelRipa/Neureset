e6e17b8e332407e134eb811a8e5b2102e369578f
Michael Ripa <michaelripa@cmail.carleton.ca>
2024-04-15 16:05:51 -0400

    Implemented treatment and wavesampling related functions for Electrode class

--- src/electrode.cpp
+++ src/electrode.cpp
@@ -10,12 +10,23 @@ void Electrode::initializeElectrode() {
 
 void Electrode::computeBaselineFrequencies() {
   // Computes the baseline frequencies for each wave
+  for (size_t i = 0; i < waves.size(); ++i) {
+    waves[i].calculateDominantFrequency();
+  }
 }
 
 void Electrode::applyTreatmentToWaves(float offset) {
   // Applies the treatment to the waves
+  for (size_t i = 0; i < waves.size(); ++i) {
+    waves[i].applyFrequencyOffset(offset);
+  }
 }
 
 std::vector<std::vector<float>> Electrode::collectWaveData(int numPointsPerWave) const {
   // Collects data from all waves for the current time point for plotting or analysis
+  std::vector<std::vector<float>> waveData(waves.size());
+  for (size_t i = 0; i < waves.size(); ++i) {
+    waveData[i] = waves[i].getCurrentWaveform(numPointsPerWave);
+  }
+  return waveData;
 }
